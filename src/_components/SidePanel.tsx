51aa628b0eb6351436bc4ef3ab6e6fb1f822a29e
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-04-20 11:09:48 +0200

    Create CustomButton component

--- src/_components/SidePanel.tsx
+++ src/_components/SidePanel.tsx
@@ -1,9 +1,11 @@
+import CustomButton from "@/components/ui/CustomButton";
+
 export default function SidePanel() {
   return (
-    <aside className="sticky top-0 w-96 border-r border-border/40 bg-zinc-950">
+    <aside className="sticky top-0 w-96 max-w-96 border-r border-border/40 bg-zinc-950 px-4">
       <div className="item-center flex h-full flex-col gap-4 py-4">
-        <button>Dashboard</button>
-        <button>My Tests</button>
+        <CustomButton href="/dashboard">Dashboard</CustomButton>
+        <CustomButton href="/my-test">My Tests</CustomButton>
       </div>
     </aside>
   );
