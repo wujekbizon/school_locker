81ebabab48882b2b43f9e00746c18ff63198dc39
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-04-20 08:24:51 +0200

    Final version of TopNav, add shadcn/ui

--- src/_components/TopNav.tsx
+++ src/_components/TopNav.tsx
@@ -1,19 +1,31 @@
+import NavButton from "@/components/ui/NavButton";
+
 export default function TopNav() {
   return (
-    <nav className="flex h-20 items-center justify-between bg-zinc-950 p-4">
-      <h1 className="border-t border-zinc-800 text-2xl text-white">
-        School <span className="text-zinc-600">Locker</span>{" "}
-      </h1>
-      <div className="flex items-center gap-4">
-        <div className="text-white">Sign in</div>
-        <div>
-          <input
-            type="text"
-            className="h-7 rounded bg-zinc-800 p-2 text-lg text-white outline-none"
-            placeholder="Search..."
-          />
+    <header className="supports-\[backdrop-filter\]\:bg-background\/60 sticky top-0 z-50 flex w-full border-b border-border/40 bg-zinc-950 backdrop-blur">
+      <div className="container flex h-14 max-w-screen-2xl items-center gap-4">
+        <div className="mr-4 hidden flex-1 md:flex">
+          <h1 className="border-t border-zinc-800 text-2xl text-white">
+            School <span className="text-zinc-600">Locker</span>{" "}
+          </h1>
+        </div>
+        <NavButton />
+        <div className="flex flex-1 items-center justify-between gap-4 md:justify-end">
+          <div className="relative flex w-full items-center justify-end ">
+            <input
+              type="text"
+              className="inline-flex h-8 w-full items-center justify-start whitespace-nowrap rounded-[0.5rem] border border-input bg-transparent  px-4 py-2 text-sm font-normal text-muted-foreground shadow-none transition-colors hover:bg-accent hover:text-white focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:pr-12 md:w-40 lg:w-64"
+              placeholder="Search..."
+            />
+            <kbd className="pointer-events-none absolute right-[0.4rem] top-[0.3rem] hidden h-5 select-none items-center gap-1 rounded border-black bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+              <span className="text-xs">⌘</span>K
+            </kbd>
+          </div>
+          <div className="flex w-20 items-center justify-center text-white">
+            Sign in
+          </div>
         </div>
       </div>
-    </nav>
+    </header>
   );
 }
