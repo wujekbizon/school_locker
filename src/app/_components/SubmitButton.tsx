bc5025a7f6984dccc21a6b01a8d1211a7bb1b459
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2024-05-24 17:31:47 +0200

    Add improvments to QuestionCard component

--- src/app/_components/SubmitButton.tsx
+++ src/app/_components/SubmitButton.tsx
@@ -12,7 +12,7 @@ const SubmitButton = ({ label, loading, disabled }: SubmitButtonProps) => {
   return (
     <button
       disabled={pending || disabled}
-      className="inline-flex h-9 w-full items-center justify-center whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-amber-400/80  focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+      className="inline-flex h-10 w-full items-center justify-center whitespace-nowrap rounded-md bg-stone-300 px-4 py-2 text-base font-medium text-black shadow transition-colors hover:bg-amber-400/80  focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
     >
       {pending ? loading : label}
     </button>
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -16,37 +16,37 @@ export default function QuestionCard(props: {
   };
 
   return (
-    <div className="relative w-full rounded-lg border border-border/40 bg-zinc-950 px-4 py-8 text-white">
+    <div className="relative flex h-full min-h-80 w-full flex-col rounded-lg border border-border/40 bg-zinc-950 px-4 py-6 text-gray-200">
       <p className="absolute right-2 top-1 text-sm text-muted-foreground">
         {props.questionNumber}
       </p>
-      <h3 className="border-b border-border/40 px-4 pb-4 text-lg">
+      <h3 className="border-b border-border/40 px-4 pb-2 text-base">
         {question}
       </h3>
-      <ul className="flex flex-col gap-2 px-4 pt-6">
+      <ul className="flex h-full flex-col  gap-2 px-4 pt-6">
         {answers.map(({ option, isCorrect }, index) => (
           <li
             key={option}
-            className="text-balance text-sm leading-relaxed text-muted-foreground"
+            className="text-balance text-sm leading-relaxed text-amber-300/40"
           >
             {LETTERS[index]}){" "}
             <span
-              className={`${showCorrectAnswer && (isCorrect ? "rounded-sm bg-zinc-600" : "opacity-25")} text-md px-2 text-zinc-200  transition-opacity`}
+              className={`${showCorrectAnswer && (isCorrect ? "rounded-sm bg-zinc-800 " : "opacity-25")} text-md px-2 text-muted-foreground  transition-opacity`}
             >
               {" "}
               {option}
             </span>
           </li>
         ))}
-        <div className="flex w-full justify-center self-center pt-4 md:w-1/2">
-          <button
-            onClick={handleCorrectAnswer}
-            className="inline-flex h-9 w-full items-center justify-center whitespace-nowrap rounded-md bg-primary/90 px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-amber-500/80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 "
-          >
-            {showCorrectAnswer ? "Hide Answer" : "Reveal Answer"}
-          </button>
-        </div>
       </ul>
+      <div className="flex w-full self-center pt-4 md:w-1/2">
+        <button
+          onClick={handleCorrectAnswer}
+          className="inline-flex h-8 w-full items-center justify-center whitespace-nowrap rounded-md bg-stone-300 px-4 py-2 text-base font-medium text-black shadow transition-colors hover:bg-amber-400/80  focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
+        >
+          {showCorrectAnswer ? "Hide Answer" : "Reveal Answer"}
+        </button>
+      </div>
     </div>
   );
 }
